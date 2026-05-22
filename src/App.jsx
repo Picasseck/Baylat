40291da05a96587a525801e1f8ca00a388bc5779
Baylat <baylatseck05@gmail.com>
2026-05-22 04:36:42 +0200

    ajout de la page à propos avec portrait et piliers

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,9 @@ import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
+import About from './pages/About';
 import Footer from './components/Footer';
 
-// Les pages viendront ici au fur et à mesure
-function Placeholder({ title }) {
-  return <div className="page container"><h1>{title}</h1></div>;
-}
 
 function App() {
   return (
@@ -24,7 +21,7 @@ function App() {
           <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
-          <Route path="/about" element={<Placeholder title="About" />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
       </BrowserRouter>
