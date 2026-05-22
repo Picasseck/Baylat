8e9ccccabf1f6e692b32ed9ae4ec0ce30fe0e771
Baylat <baylatseck05@gmail.com>
2026-05-22 04:14:02 +0200

    ajout de la page commande avec validation formulaire (#10)

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
 import Footer from './components/Footer';
 
 // Les pages viendront ici au fur et à mesure
@@ -22,6 +23,7 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/about" element={<Placeholder title="About" />} />
         </Routes>
         <Footer />
